3cbc02ead681512b3f9aa34f00fa7d8a90a05b72
ogoley <ogoleyj@gmail.com>
2020-06-20 12:31:44 -0400

    Updates

---
+++ precache-manifest.679e48b48c3635173348825ea3af22b7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fab550f7116266c19cb961780765ca8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ea3effcad4e8f3ab04b4",
+    "url": "/portfolio/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "a3152a76f06086405b90",
+    "url": "/portfolio/static/css/main.0d434d54.chunk.css"
+  },
+  {
+    "revision": "ea3effcad4e8f3ab04b4",
+    "url": "/portfolio/static/js/2.d5916d06.chunk.js"
+  },
+  {
+    "revision": "a3152a76f06086405b90",
+    "url": "/portfolio/static/js/main.180fe6dd.chunk.js"
+  },
+  {
+    "revision": "ba291b4c38efa56919e7",
+    "url": "/portfolio/static/js/runtime-main.d73ac330.js"
+  },
+  {
+    "revision": "9a9cab82ac0c8df725576a0dd54d9520",
+    "url": "/portfolio/static/media/stats.9a9cab82.jpg"
+  },
+  {
+    "revision": "6695f8d241eee86ce46a5fd3d25d1486",
+    "url": "/portfolio/static/media/sunset.6695f8d2.jpg"
+  },
+  {
+    "revision": "e6b5de8e170a2e8acaec98979d01feb6",
+    "url": "/portfolio/static/media/work.e6b5de8e.jpg"
+  }
+]);
